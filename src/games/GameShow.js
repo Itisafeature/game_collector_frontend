13154d51ac04a66560c5eb7a8290ec0305a27330
Itisafeature <thebigdebugger@gmail.com>
2021-10-02 19:51:50 -0400

    format ratings for pie chart

--- src/games/GameShow.js
+++ src/games/GameShow.js
@@ -1,15 +1,34 @@
 import { useSelector } from 'react-redux';
+import Pie, { ProvidedProps, PieArcDatum } from '@visx/shape/lib/shapes/Pie';
+import { Group } from '@visx/group';
 
 const GameShow = ({ match }) => {
   const game = useSelector(state =>
     state.games.allGames.find(game => game.slug === match.params.slug)
   );
 
-  console.log('here');
-  console.log(game);
+  const formatRatings = () => {
+    const { id, gameId, total, ...ratings } = game.rating;
+    let ratingsArr = [];
+
+    for (const key in ratings) {
+      ratingsArr.push({ type: key, value: ratings[key] });
+    }
+    return ratingsArr;
+  };
+
+  const width = 0.2;
+  const half = width * 0.5;
+  const ratingsArr = formatRatings();
+
   return (
     <div>
       <h1>{game.title}</h1>
+      <svg width={width} height="20%">
+        <Group top={half} half={half}>
+          <Pie date={ratingsArr} pieValue={data => data.value}></Pie>
+        </Group>
+      </svg>
     </div>
   );
 };
