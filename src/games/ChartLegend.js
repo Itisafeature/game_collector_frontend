0723bfd9eb7c20fa555a45e091a36510190df50e
Itisafeature <thebigdebugger@gmail.com>
2021-10-05 19:13:16 -0400

    styled games show

--- src/games/ChartLegend.js
+++ src/games/ChartLegend.js
@@ -2,13 +2,15 @@ import styled from 'styled-components';
 
 const LegendTable = styled.table`
   border: 1px solid black;
+  align-self: center;
+  padding: 10px;
+  margin-right: 10px;
 `;
 
 const LegendRow = styled.tr`
   color: ${props => props.color};
 `;
 const ChartLegend = ({ ratings }) => {
-  console.log(ratings);
   return (
     <LegendTable>
       {ratings.map(ratings => (
--- src/games/GameShow.js
+++ src/games/GameShow.js
@@ -1,20 +1,31 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 import Pie, { ProvidedProps, PieArcDatum } from '@visx/shape/lib/shapes/Pie';
 import { Group } from '@visx/group';
 import { scaleOrdinal } from '@visx/scale';
 import { Text } from '@visx/text';
 import ChartLegend from './ChartLegend';
 
+const ShowContainerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ChartDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
 const GameShow = ({ match }) => {
-  const [active, setActive] = useState(null);
   const game = useSelector(state =>
     state.games.allGames.find(game => game.slug === match.params.slug)
   );
 
   const formatRatings = () => {
     const { id, gameId, total, ...ratings } = game.rating;
-    const colors = ['blue', 'red', 'purple', 'green', 'orange'];
+    const colors = ['orange', 'red', 'purple', 'green', 'orange'];
     let ratingsArr = [];
 
     let colorIndex = 0;
@@ -33,71 +44,64 @@ const GameShow = ({ match }) => {
   const half = width / 2;
   const ratingsArr = formatRatings();
 
-  // const getRatingsFrequency = scaleOrdinal({
-  //   domain: ratingsArr.map(r => r.value),
-  //   range: ['red', 'green', 'rgba(93,30,91,0.6)', 'rgba(93,30,91,0.4)'],
-  // });
-
   return (
-    <div>
+    <ShowContainerDiv>
       <h1>{game.title}</h1>
-      <ChartLegend ratings={ratingsArr} />
-
-      <svg width={width} height={width}>
-        <Group top={half} left={half}>
-          <Pie
-            data={ratingsArr}
-            pieValue={data => data.value}
-            outerRadius={half}
-          >
-            {pie => {
-              return pie.arcs.map(arc => {
-                const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
-                const [centroidX, centroidY] = pie.path.centroid(arc);
-                const arcFill = arc.data.color;
-                const percent =
-                  ((arc.data.value / game.rating.total) * 100).toFixed() + '%';
-                return (
-                  <g
-                    key={arc.data.type}
-                    onMouseEnter={() => setActive(arc.data)}
-                    onMouseLeave={() => setActive(null)}
-                  >
-                    <path d={pie.path(arc)} fill={arcFill} />
-                    {hasSpaceForLabel && (
-                      <>
-                        <Text
-                          x={centroidX}
-                          y={centroidY}
-                          dy=".33em"
-                          fill="#ffffff"
-                          fontSize={22}
-                          textAnchor="middle"
-                          pointerEvents="none"
-                        >
-                          {arc.data.value}
-                        </Text>
-                        <Text
-                          x={centroidX + 5}
-                          y={centroidY + 30}
-                          dy=".33em"
-                          fill="#ffffff"
-                          fontSize={22}
-                          textAnchor="middle"
-                          pointerEvents="none"
-                        >
-                          {percent}
-                        </Text>
-                      </>
-                    )}
-                  </g>
-                );
-              });
-            }}
-          </Pie>
-        </Group>
-      </svg>
-    </div>
+      <ChartDiv>
+        <ChartLegend ratings={ratingsArr} />
+        <svg width={width} height={width}>
+          <Group top={half} left={half}>
+            <Pie
+              data={ratingsArr}
+              pieValue={data => data.value}
+              outerRadius={half}
+            >
+              {pie => {
+                return pie.arcs.map(arc => {
+                  const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
+                  const [centroidX, centroidY] = pie.path.centroid(arc);
+                  const arcFill = arc.data.color;
+                  const percent =
+                    ((arc.data.value / game.rating.total) * 100).toFixed() +
+                    '%';
+                  return (
+                    <g key={arc.data.type}>
+                      <path d={pie.path(arc)} fill={arcFill} />
+                      {hasSpaceForLabel && (
+                        <>
+                          <Text
+                            x={centroidX}
+                            y={centroidY}
+                            dy=".33em"
+                            fill="#ffffff"
+                            fontSize={22}
+                            textAnchor="middle"
+                            pointerEvents="none"
+                          >
+                            {arc.data.value}
+                          </Text>
+                          <Text
+                            x={centroidX + 5}
+                            y={centroidY + 30}
+                            dy=".33em"
+                            fill="#ffffff"
+                            fontSize={22}
+                            textAnchor="middle"
+                            pointerEvents="none"
+                          >
+                            {percent}
+                          </Text>
+                        </>
+                      )}
+                    </g>
+                  );
+                });
+              }}
+            </Pie>
+          </Group>
+        </svg>
+      </ChartDiv>
+    </ShowContainerDiv>
   );
 };
 
